21f8ce7ecb66273df51c24ed7874c5c680581a21
Sergey Sergeev <serdgik8@mail.ru>
2019-07-14 06:07:33 +0300

    Add setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup 
+
+
+setup(
+	name="mycalc",
+	version="0.0.1",
+	description="A small example package with some arithmetics",
+	packages=find_packages(),
+	include_package_data=True,
+	author="Serg",
+	author_email="Serg@example,com",
+	url="https://github.com/Serg2458/mycalc",
+	)
